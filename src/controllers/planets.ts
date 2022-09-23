f2668043f4516b7325839c4ef11f097077e58165
Christofer Castillo <ccastillo993a@gmail.com>
2022-09-23 13:07:40 -0500

    feat: creación de servicio para obtener un planeta

--- src/controllers/planets.ts
+++ src/controllers/planets.ts
@@ -1,25 +1,10 @@
-import { PrismaClient } from '@prisma/client'
 import { Request, Response } from 'express'
-
-const prisma = new PrismaClient()
+import planetService from 'services/planets'
 
 export const getFindOne = async (req: Request, res: Response) => {
 	const { name } = req.params
+	const planet = await planetService.getPlanet(name)
 
-	const planet = await prisma.planet.findFirst({
-		where: {
-			name: {
-				startsWith: name,
-				mode: 'insensitive',
-			},
-		},
-		include: {
-			geology: true,
-			images: true,
-			overview: true,
-			structure: true,
-		},
-	})
 	if (!planet) return res.status(404).send(`Not found this planet: ${name}`)
 
 	return res.json(planet)
